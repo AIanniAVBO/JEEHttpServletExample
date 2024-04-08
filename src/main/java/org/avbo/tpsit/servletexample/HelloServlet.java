83abcaef69572b8b288ed282b779d45fa8475fa1
Andrea Iannì <andrea.ianni@avbo.it>
2024-04-08 10:27:40 +0200

    Creata una semplice servlet che saluta in base al parametro impostato

---
+++ src/main/java/org/avbo/tpsit/servletexample/HelloServlet.java
@@ -0,0 +1,47 @@
+package org.avbo.tpsit.servletexample;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		if (request.getParameter("name")!=null)
+		{
+			response.getWriter().append("Ciao ")
+					.append(request.getParameter("name")) 
+					.append(", sono una servlet che saluta.");
+		}
+		else
+			response.getWriter().append("Parametro name non impostato, non ti posso salutare");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
